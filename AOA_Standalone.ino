ee2d9581fa019be70ffbfccc07fc1ce3b2a2d47a
Experimental Avionics <oleg@experimentalavionics.com>
2019-04-21 17:46:02 +1000

    Logic was fully rebuild after field test

--- AOA_Standalone.ino
+++ AOA_Standalone.ino
@@ -6,47 +6,43 @@
 // or  MPXV5010 - 10 kPa (~248 Knots)
 //     This one might be a bit too rough for the AoA sensor but good for Airspeed sensor
 //
-// A bit of theory (magic)
-// Assume
-// z - Angle of attack
-// y - angle between pitot and AoA port
-// V - airspeed
-// D = differential AoA airspeed (I know this one is weird, but bear with me)
-// D = V*(cos(z) - cos(y-z))
-// Here where we cheat: (cos(z) - cos(y-z)) non-linear, but in the range of z [-5..+15] it is practically linear so can be converted to (a*z+b)
-// a = (cos(15)-cos(y-15))/15 - linear approximation between 0deg and 15deg
-// b = cos(y)
-// so D = V(az+b)
-// Now, D is measured by the AoA sensor
-// V is measured by the Airspeed sensor. At this stage we assume V doesnt change with the change of AoA. In reality there is 3% variation on the range of z [-5..+15]
-// Finally z = (D/V-b)/a
-// Linear approximation parameters a and b will be calculated at startup based on Y and stall AoA
-// However, D/V is equvalent to SQRT(Paoa/Pias) because of the formula V=SQRT(2*Pias/R) where R is air density
-// So finally z = (SQRT(Paoa/Pias)-b)/a
+// For details visit: http://experimentalavionics.com/angle-of-attack-standalone-unit/
+
+// V=SQRT(2*P/R) where P - dinamic pressure, R is air density (1.225kg/m3)
 
 int i = 0; // counter
 unsigned long currentMillis;
+unsigned long LastOutputTS;
+int Fault = 0; // Error code
+                // 1 - blocked port (one of them)
+
+// flaps change airfoil so the calibration data will be different for each flaps position
+#define FlapsArraySize 4      // Number of flaps positions (eg 0, 5, 10, 30deg)
+float Xzero[FlapsArraySize];  // Pias/Paoa at Zero AoA for each flaps position. Xzero[0] - no flaps
+float Xstall[FlapsArraySize]; // Pias/Paoa at Stall AoA for each flaps position. Xstall[0] - no flaps
+int FlapsPosition = 0;        // Flaps position 0 - no flaps, 1 - first stage, etc
+
 float AOA = 0;
-int AOAmin = -2;
-int AOAmax = 16;   //Stall AoA
-float a_lap, b_lap; // linear approximation params 
+float Xaoa = 0;    // Temporary variable for Pias/Paoa
+float AOAmin = -2;   // Min AoA (for display - turns blue and green LEDs on at Zero AoA)
+float AOAmax = 15;   // Stall AoA
+float AOAbg = 4.8;     // Best Glide AoA (Top Green LED is on)
+float A, B;        // linear funclion coefficients for calculating AoA = A*Xaoa + B
 
 int topLED = 2;
 
-unsigned int SensorAngle = 62; //Angle between the pittot port and the AOA port (degrees)
-float Pmin = 100; // Minimum differeretial pressure to activate the unit. 
-                  // Corresponds to about 25 knots
+float Pmin = 15; // Minimum differeretial pressure to activate the unit. 
 
 // AoA differential sensor
 unsigned int AOArefMin = 5;    // Minimum reading from the sensor. For some reason it is never 0
-unsigned int AOArefHalf = 534; // Sensor reading when AOA is half the angle between pitot and AOA port
+unsigned int AOArefHalf = 534; // Sensor reading at Zero airspeed
 unsigned int AOArefMax = 1015; // Maximum reading from the sensor. For some reason it is never 1023
 unsigned int AOAMaxPressure = 2000; // max differential pressure for the installed sensor
                                     // for MPXV7002 - 2000 Pa (~110 Knots)
                                     // for MPXV5010 - 10000 pa (~248 Knots)
 // IAS differential sensor
 unsigned int IASrefMin = 6;    // Minimum reading from the sensor. For some reason it is never 0
-unsigned int IASrefHalf = 537; // Sensor reading when AOA is half the angle between pitot and AOA port
+unsigned int IASrefHalf = 535; // Sensor reading at Zero airspeed
 unsigned int IASrefMax = 1015; // Maximum reading from the sensor. For some reason it is never 1023
 unsigned int IASMaxPressure = 2000; // max differential pressure for the installed sensor
                                     // for MPXV7002 - 2000 Pa (~110 Knots)
@@ -57,50 +53,89 @@ unsigned int IAS_RAW_Input = 0;
 float Pias = 0, Paoa = 0; // Differential pressure from the sensors
 
 // two arrays for moving avarage
-#define ArraySize 8 // play with the value - greater value display is less sensitive to change, smaller value - display becomes jumpy
-float PiasArray[ArraySize];
-float PiasArraySum =0;
-float PaoaArray[ArraySize];
-float PaoaArraySum =0;
-int ArrayIndex = 0;
+#define ArraySize 300 // play with the value - greater value display is less sensitive to change, smaller value - display becomes jumpy
+unsigned int RAWiasArray[ArraySize];
+unsigned long RAWiasArraySum =0;
+unsigned int RAWaoaArray[ArraySize];
+unsigned long RAWaoaArraySum =0;
+unsigned int ArrayIndex = 0;
 
 void setup() 
 {
    Serial.begin(115200);    
 // init the pins 2-11
-  for (int i = 2; i <= 11; i++) {
+  for (int i = 2; i <= 12; i++) {
     pinMode(i, OUTPUT);  
     delay(10);
   }   
 
   delay(300);
-
-  for (int i = 2; i <= 11; i++) {
+  
+  for (int i = 2; i <= 12; i++) {
     digitalWrite(i, HIGH); 
     delay(200);
   }   
 
-  // calculate linear approximation params
-  a_lap = (cos((float)AOAmax*PI/180)-cos((float)(SensorAngle-AOAmax)*PI/180))/(float)AOAmax;
-  b_lap = cos((float)SensorAngle*PI/180);
+  digitalWrite(12, LOW); 
 
-  Serial.print("a_lap = ");
-  Serial.println(a_lap);
-  Serial.print("b_lap = ");
-  Serial.println(b_lap);
+// Read the calibrations values from EEPROM and calculate the linear function coefficients
+// hardcoded values for now
+   Xzero[0] = 1.6934;
+   Xstall[0] = 1.967;
+
+   Xzero[1] = 1.6934;
+   Xstall[1] = 1.967;
+
+   Xzero[2] = 1.6934;
+   Xstall[2] = 1.967;
+
+   Xzero[3] = 1.6934;
+   Xstall[3] = 1.967;
+
+   Serial.print("AOA_RAW");
+   Serial.print("\t");
+   Serial.print("IAS_RAW");
+   Serial.print("\t");
+   Serial.print("Paoa");
+   Serial.print("\t");
+   Serial.print("Pias");
+   Serial.print("\t");
+   Serial.print("IAS");
+   Serial.print("\t");
+   Serial.println("AOA");
+
 }
 
 void loop()
 {
 
- AOA_RAW_Input = analogRead(A1); 
- Serial.print("AOA_RAW_Input = ");
- Serial.println(AOA_RAW_Input);
+// Read Flaps position  
+ FlapsPosition = 0;
 
- IAS_RAW_Input = analogRead(A7); 
- Serial.print("IAS_RAW_Input = ");
- Serial.println(IAS_RAW_Input);
+ A = (float)AOAmax / (Xstall[FlapsPosition] - Xzero[FlapsPosition]);
+ B = (-1) * A * Xzero[FlapsPosition];
 
+// Read sensors
+  IAS_RAW_Input = analogRead(A7); 
+
+  RAWiasArraySum -= RAWiasArray[ArrayIndex];
+  RAWiasArray[ArrayIndex] = IAS_RAW_Input;
+  RAWiasArraySum += RAWiasArray[ArrayIndex];
+  IAS_RAW_Input = RAWiasArraySum / ArraySize;
+
+  AOA_RAW_Input = analogRead(A1); 
+
+  RAWaoaArraySum -= RAWaoaArray[ArrayIndex];
+  RAWaoaArray[ArrayIndex] = AOA_RAW_Input;
+  RAWaoaArraySum += RAWaoaArray[ArrayIndex];
+  AOA_RAW_Input = RAWaoaArraySum / ArraySize;
+
+  ArrayIndex++; // shift the index for next time
+  if (ArrayIndex == ArraySize) {  // if we reached the top of the array
+    ArrayIndex = 0;                       //go to the start of the array
+  }
+
+  
  // the "if" below is to compensate for non-linearity and disbalance of the pressure sensor
  if (AOA_RAW_Input > AOArefHalf) {
     Paoa = (float)AOAMaxPressure * ((float)AOA_RAW_Input - (float)AOArefHalf) / ((float)AOArefMax - (float)AOArefHalf);
@@ -114,47 +149,39 @@ void loop()
     Pias = (float)IASMaxPressure * ((float)IAS_RAW_Input - (float)IASrefHalf) / ((float)IASrefHalf - (float)IASrefMin);
  }
 
-  PiasArraySum -= PiasArray[ArrayIndex];
-  PiasArray[ArrayIndex] = Pias;
-  PiasArraySum += PiasArray[ArrayIndex];
-  Pias = PiasArraySum / ArraySize;
+ Paoa=(-1)*Paoa;  // either this or swap around the ports on the sensor
 
- Serial.print("Paoa = ");
- Serial.println(Paoa);
- Serial.print("Pias = ");
- Serial.println(Pias);
 
-  PaoaArraySum -= PaoaArray[ArrayIndex];
-  PaoaArray[ArrayIndex] = Paoa;
-  PaoaArraySum += PaoaArray[ArrayIndex];
-  Paoa = PaoaArraySum / ArraySize;
-
-  ArrayIndex +=1; // shift the index for next time
-  if (ArrayIndex == ArraySize) {  // if we reached the top of the array
-    ArrayIndex = 0;                       //go to the start of the array
-  }
-
- 
- Serial.print("Paoa = ");
- Serial.println(Paoa);
- Serial.print("Pias = ");
- Serial.println(Pias);
- Serial.print("IAS = ");
- Serial.println(3600.0*sqrt(2*Pias/1.225)/1852.0);
- Serial.println();
-
- if (Pias > Pmin and Paoa > 0) {
-    AOA = (sqrt(Paoa/Pias)-b_lap)/a_lap;
+ if (Pias > Pmin) {
+    Xaoa = Pias/Paoa;
+    if (abs(Xaoa)>15) {  // large value indicates blocked port somewhere
+      AOA = AOAmax;
+      Fault = 1;
+    } else {
+      Fault = 0;
+      AOA = round(A*Xaoa + B);
+    }
  } else {
+    Fault = 0;
+    Xaoa = 0;
     AOA = 0;
  }
- 
- Serial.print("AOA = ");
- Serial.println(AOA);
- Serial.println();
 
-  topLED = map((int)AOA, AOAmin, AOAmax, 11, 2); // map doesnt work properly
+  if (AOA < AOAmin) {
+    AOA = AOAmin;
+  }
+  
+  if (AOA > AOAmax) {
+    AOA = AOAmax;
+  }
 
+  if (AOA > AOAbg) {
+    topLED = map(AOA,AOAbg, AOAmax, 6, 2); // map function is weird sometimes. Watch out!
+  } else {
+    topLED = map(AOA, AOAmin, AOAbg, 11, 7); // map function is weird sometimes. Watch out!
+  }
+
+  
   for (int i = topLED; i <= 11; i++) {
     digitalWrite(i, LOW); 
   }
@@ -163,6 +190,25 @@ void loop()
     digitalWrite(i, HIGH); 
   }
   
-  delay(100);
+// serial output every xxx ms
+
+  if (millis() - LastOutputTS > 1000) {
+
+      Serial.print(AOA_RAW_Input);
+      Serial.print("\t");
+      Serial.print(IAS_RAW_Input);
+      Serial.print("\t");
+      Serial.print(Paoa);
+      Serial.print("\t");
+      Serial.print(Pias);
+      Serial.print("\t");
+      Serial.print(3600.0*sqrt(2*Pias/1.225)/1852.0);
+      Serial.print("\t");
+      Serial.println(AOA);
+
+      LastOutputTS = millis();
+
+  }
+
  
 }
